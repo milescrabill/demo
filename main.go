7847e36e1382886d8e42796bc43b9072e0e1d068
Miles Crabill <mcrabill@mozilla.com>
2018-05-01 20:48:56 -0700

    initial commit w/ dockerfile

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "<h1>hello, world!</h1>\n")
+	})
+	http.ListenAndServe(":8080", nil)
+}
