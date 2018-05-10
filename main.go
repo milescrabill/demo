991e2f50cfde14fe5905fd94ecbc167e60e41ddb
Miles Crabill <mcrabill@mozilla.com>
2018-05-10 15:10:24 -0400

    hello bob

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// this is a comment
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "<h1>hello, world!</h1>\n")
+		io.WriteString(w, "<h1>hello, bob!</h1>\n")
 	})
 	http.ListenAndServe(":5000", nil)
 }
